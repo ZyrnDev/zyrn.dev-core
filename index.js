683249235b393c6f18373e63b646d8f4e74bd0b3
Mitchell Lee <development@zyrn.dev>
2020-04-25 20:16:16 +1000

    Added server-side rendering with EJS

--- index.js
+++ index.js
@@ -4,16 +4,24 @@ const bodyParser    = require("body-parser");
 const createError   = require('http-errors');
 const cors          = require('cors');
 const fs            = require('fs');
+const path          = require('path');
 
 // Constants and global scoped variables
 const app = express();
 global.config = JSON.parse(fs.readFileSync('settings.json', 'utf8'));
 global.database = require('./src/database.js');
 
+// Configure Express Settings
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Add the primary route
 app.use('/', require('./routes/core.js'));
 
 //Catch Errors
@@ -23,21 +31,26 @@ app.use(function(req, res, next) {
 
 // Error handler
 app.use(function(err, req, res, next) {
-    res.locals.message = err.message;
-    res.writeHead(err.status || 500, {'Content-Type': 'application/json'});
 
     let error = {
-        status: err.status,
+        status: err.status || 500,
         message: err.message,
         request: {
             method: req.method,
             uri: req.headers.host + req.url,
-            body: JSON.stringify(req.body)
+            body: req.body
         }
     };
-    
-    res.write(JSON.stringify(error));
-    res.end();
+
+    // If the request was to the API send a json error, otherwise use a human readable one
+    if (req.url.toLowerCase().startsWith("/api")) {
+        res.locals.message = err.message;
+        res.writeHead(err.status || 500, {'Content-Type': 'application/json'});
+        res.write(JSON.stringify(error));
+        res.end();
+    } else {
+        res.render('error', { page: { title: error.status + " " + error.message }, error: error })
+    }
 });
 
 app.listen(global.config.port, () => console.log(`Server Listening at: http://localhost:${global.config.port}`));
--- routes/core.js
+++ routes/core.js
@@ -2,8 +2,7 @@ const express   = require('express');
 const router    = express.Router();
 
 router.get('/', function (req, res) {
-    res.send('<form action="http://localhost:9000/api/realm/accounts/add" method="POST"><div><label>Email</label><input type="text" name="guid"></div><div><label>Password</label><input type="text" name="password"></div><input type="submit" class="my-button" value="Add"></form>');
-    // res.send('<div id="account"> <form action="http://localhost:9000/api/realm/accounts/update" method="POST"> <label>ID:</label>29<br> <input type="hidden" name="id" value=29> <label>Email:</label><input type="text" name="guid" value="email"/><br> <label>Password:</label><input type="text" name="password" value=""/><br> <input type="submit" class="my-button" value="Submit"> </form> <form action="http://localhost:9000/api/realm/accounts/delete/29" method="POST"> <input type="hidden" name="id" value=29> <input type="submit" class="my-button" value="Delete"> </form> </div>');
+    res.render("index", { page: { title: "Home" } });
 });
 
 //Routes
