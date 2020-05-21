607561a5e0cc9ea3bb7d7b2b762eb15da9cc2c74
Mitchell Lee <development@zyrn.dev>
2020-05-21 23:18:59 +1000

    Added a joke for Sam

--- routes/jokes/core.js
+++ routes/jokes/core.js
@@ -4,5 +4,8 @@ const router    = express.Router();
 router.get('/pawi', function (req, res) {
     res.render('templates/centered', { page: { title: "Pawi" }, content: { body: 'jokes/pawi' } });
 });
+router.get('/flower', function (req, res) {
+    res.render('templates/centered', { page: { title: "Sam's Flower" }, content: { body: 'jokes/flower' } });
+});
 
 module.exports = router;
